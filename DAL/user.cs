877fd1279e60030fbd94cf5990596fa892915bd7
何建钦 <1373883296@qq.com>
2016-10-15 21:22:51 +0800

    添加数据库，实现登陆

--- DAL/user.cs
+++ DAL/user.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class user
+    public class User
     {
         public string username { get; set; }
         public string password { get; set; }
+         public User(string username) //构造函数
+        {
+            DataTable all_info = SqlHelper.ExecuteDataTable(
+                "select * from T_Users where username=@username",
+                new SqlParameter("@username", username)
+                );
+            if (all_info.Rows.Count == 0)
+            {
+                return;
+            }
+            this.username = all_info.Rows[0]["username"].ToString();
+            this.password = all_info.Rows[0]["password"].ToString();
+        }
+
+        public int Login(string paw) //登陆
+        {
+            if (this == null)
+            {
+                return 0;
+            }
+            if (paw == this.password)
+            {
+                return 1;
+            }
+            return 2;
+        }
     }
 }
